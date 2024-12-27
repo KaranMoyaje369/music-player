2b24b5f8fca3afe96026d8262a15a8cf10bdd8a8
KaranMoyaje369 <karanmoyaje369@gmail.com>
2024-12-27 11:59:07 +0530

    Music Player

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
     outDir: "build", // This makes sure Vite outputs to the 'build' folder
   },
+  base: "/music-player/", // Add this line
+  plugins: [react()],
 });
